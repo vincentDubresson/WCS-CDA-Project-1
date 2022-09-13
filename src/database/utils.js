fbadd0de0080503d3c26dd2817cf4ce734516d0e
vincentDubresson <vincent.dubresson@orange.fr>
2022-09-13 11:17:10 +0200

    Refact-1 : Move creation of the database in a new folder/file

---
+++ src/database/utils.js
@@ -0,0 +1,16 @@
+const typeorm = require('typeorm');
+const Wilder = require("../models/Wilder");
+
+// On crée ou on va chercher la base de données.
+const dataSource = new typeorm.DataSource({
+  type: "sqlite",
+  database: "./WCS-CDA-Project-1.sqlite",
+  // Ne jamais mettre l'option ci-dessous en prod
+  synchronize: true,
+  // On déclare l'entité utilisée dans la base de données.
+  entities: [Wilder],
+  // Ne jamais mettre l'option ci-dessous en prod
+  logging: ["query", "error"],
+});
+
+exports.dataSource = dataSource;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 // On appelle Express
 const express = require('express');
-
 // On appelle TypeORM
-const typeorm = require('typeorm');
-
+const { dataSource } = require("./database/utils");
 // On appelle l'entité
 const Wilder = require("./models/Wilder");
 
 // On crée le Serveur
 const app = express();
-
-// On crée ou on va chercher la base de données.
-const dataSource = new typeorm.DataSource({
-  type: "sqlite",
-  database: "./WCS-CDA-Project-1.sqlite",
-  // Ne jamais mettre l'option ci-dessous en prod
-  synchronize: true,
-  // On déclare l'entité utilisée dans la base de données.
-  entities: [Wilder],
-  // Ne jamais mettre l'option ci-dessous en prod
-  logging: ["query", "error"],
-})
 
 // On récupère la route
 app.get('/', function (req, res) {
