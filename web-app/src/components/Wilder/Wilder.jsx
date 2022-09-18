c1f0838c44252bbf124a29e4fec3cd0c388090ad
vincentDubresson <vincent.dubresson@orange.fr>
2022-09-18 16:59:12 +0200

    Creation of a new component WIlderCardOptions with two link for update and deletion + SCSS integration in each card

--- web-app/src/components/Wilder/Wilder.jsx
+++ web-app/src/components/Wilder/Wilder.jsx
@@ -1,6 +1,9 @@
 import Skill from '../Skill/Skill';
 import TeachStamp from '../../assets/images/TeacherStamp.png';
 import './Wilder.scss';
+import moreHorizon from '../../assets/icons/moreHorizon.png';
+import WilderCardOptions from '../WilderCardOptions/WilderCardOptions';
+import { useState } from 'react';
 
 export default function Wilder({
   firstName,
@@ -11,29 +14,48 @@ export default function Wilder({
   school,
   skills
 }) {
+  const [optionListClass, setOptionListClass] = useState('WilderCardOptionsContainer');
+  
+  const openWilderOptionsList = () => {
+    setOptionListClass('WilderCardOptionsContainer OptionsDisplay');  
+  }
+
+  const wilderCardOptionsCallback = (className) => {
+    setOptionListClass(className);
+  }
+
   return(
     <article className={isTeacher ? "WilderCard TeachCard" : "WilderCard"}>
-            {isTeacher ? (
-              <img className="WilderCardTeacherStamp" src={TeachStamp} alt="Teacher stamp" />
-            ) : null}
-            <img className="WilderCardPicture" src={picture} alt={`${firstName} ${lastName} profile`} />
-            <h3 className="WilderCardName">{firstName} {lastName}</h3>
-            <h4 className="WilderCardSchool">{school.schoolName}</h4>
-            <p className="WilderCardDescription">
-              {description}
-            </p>
-            <h4 className="WilderCardSkillsTitle">Wild Skills</h4>
-            <ul className="WilderCardSkillsContainer">
-              {
-                skills.map((skill) => {
-                  return (
-                    <li className="WilderCardSkills" key={skill.id}>
-                      <Skill name={skill.skillName} numberOfVotes={isTeacher ? 5 : skill.skillScore} />
-                    </li>
-                  )
-                })
-              }
-            </ul>
-          </article>
+      {isTeacher ? (
+        <img className="WilderCardTeacherStamp" src={TeachStamp} alt="Teacher stamp" />
+      ) : null}
+      <img
+        className="WilderCardOptionsIcon"
+        src={moreHorizon}
+        alt="options popup open icon"
+        onClick={() => {openWilderOptionsList()}}
+        />
+      <WilderCardOptions
+        dynamicClass={optionListClass}
+        callback={wilderCardOptionsCallback}/>
+      <img className="WilderCardPicture" src={picture} alt={`${firstName} ${lastName} profile`} />
+      <h3 className="WilderCardName">{firstName} {lastName}</h3>
+      <h4 className="WilderCardSchool">{school.schoolName}</h4>
+      <p className="WilderCardDescription">
+        {description}
+      </p>
+      <h4 className="WilderCardSkillsTitle">Wild Skills</h4>
+      <ul className="WilderCardSkillsContainer">
+        {
+          skills.map((skill) => {
+            return (
+              <li className="WilderCardSkills" key={skill.id}>
+                <Skill name={skill.skillName} numberOfVotes={isTeacher ? 5 : skill.skillScore} />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </article>
   )
 }
---
+++ web-app/src/components/WilderCardOptions/WilderCardOptions.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import './WilderCardOptions.scss';
+import closeIcon from '../../assets/icons/closeDark.png';
+import { useState } from "react";
+
+export default function WilderCardOptions({ dynamicClass, callback }) {
+  
+  const closeWilderOptionsList = () => {
+    callback("WilderCardOptionsContainer");
+  }
+
+  return(
+    <div className={dynamicClass}>
+      <img
+        className="WilderCardOptionsClose"
+        src={closeIcon}
+        alt="wilder options close icon"
+        onClick={() => {closeWilderOptionsList()}}
+        />
+      <Link className="WilderCardOptionsLink" to="/">Modifier le Wilder</Link>
+      <Link className="WilderCardOptionsLink" to="/">Supprimer le Wilder</Link>
+    </div>
+  )
+}
